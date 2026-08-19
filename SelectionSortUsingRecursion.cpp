530ce39a8ca72e1633cec14ab743f894894ce783
Aayush_Sharma <aayush21sharma2004@gmail.com>
2026-08-19 07:43:12 +0530

    Selection Sort Using Recursion

---
+++ SelectionSortUsingRecursion.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+  void sortArray(vector<int>& v, int n) {
+    if(n==0 || n==1) {
+        return;
+    }
+    int minIndex=0;
+         for(int i=0; i<n-1; i++) {
+            if(v[i]>v[i+1]) {
+                minIndex=v[i+1];
+            
+         }
+         swap(v[minIndex], v[i]);
+        
+        }
+    
+    sortArray(v,n-1);
+  }
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    
+    for(int i = 0; i<n; i++) {
+        cin>>v[i];
+    }
+    sortArray(v,n);
+
+    for(int i = 0; i<n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
