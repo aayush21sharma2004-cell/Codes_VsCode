9178d6fa56191a33b9de6eb756925e1cd2db1bd6
Aayush_Sharma <aayush21sharma2004@gmail.com>
2026-08-19 07:24:23 +0530

    Bubble sort using recursion

---
+++ Bubble_sort_using_recursion.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+  void sortArray(vector<int>& v, int n) {
+    if(n==0 || n==1) {
+        return;
+    }
+    for(int i=0; i<n; i++){
+        if(v[i]>v[i+1]) {
+            swap(v[i], v[i+1]);
+        }
+    }
+    sortArray(v,n-1);
+  }
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    
+    for(int i = 0; i<n; i++) {
+        cin>>v[i];
+    }
+    sortArray(v,n);
+
+    for(int i = 0; i<n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
